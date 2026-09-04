33d36c6cc4f30c99fc3781048c5dc6b9a73488cb
ervinCodes <ean1207@gmail.com>
2026-09-03 17:20:52 -0700

    fix: update cookie settings for mobile cross-domain auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -37,8 +37,8 @@ module.exports = {
 
             res.cookie('token', token, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'lax',
+                secure: true,
+                sameSite: 'none',
                 maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
             })
 
